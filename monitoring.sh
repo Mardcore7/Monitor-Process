5dafa3926e342babebef6b8c6e3478c2b170dcb3
Mardcore7 <83950727+Mardcore7@users.noreply.github.com>
2022-02-09 11:53:31 +0000

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,13 +1,13 @@
-    #!/bin/bash
+#!/bin/bash
 
-    #Loop by line
-    IFS=$’\n’
+#Loop by line
+IFS=$’\n’
 
-    old_process=$(ps -eo command)
+old_process=$(ps -eo command)
 
-    while true; do
+while true; do
     new_process=$(ps -eo command)
     diff <(echo “$old_process”) <(echo “$new_process”) |grep [\<\>]
     sleep 1
     old_process=$new_process
-    done
+done
